b6d258b004ace51b342bb9e9084ac4a773e4201d
annie0sc <annie0sc@gmail.com>
2021-01-19 13:05:31 -0600

    Added some changes to the messages.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//import { QuestModule } from './quest/quest.module';
 
 @Module({
+//  imports: [QuestModule],
   imports: [],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! - Annie Chandolu';
   }
   getAbout(): string {
-    return 'Annie Software Developer.';
+    return 'Hello this is an example of nest.js API.';
   }
 }
