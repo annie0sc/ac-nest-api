9d5782447112200c57fa8caabd077a4bb4e9ab51
annie0sc <annie0sc@gmail.com>
2021-01-21 21:17:39 -0600

    Added quest resources.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { QuestModule } from './quest/quest.module';
+import { QuestModule } from './quest/quest.module';
 
 @Module({
-//  imports: [QuestModule],
-  imports: [],
+  imports: [QuestModule],
   controllers: [AppController],
   providers: [AppService],
 })
