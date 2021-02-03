e1e6d8cec58340e84611d466f40e968b6aebd7fc
annie0sc <annie0sc@gmail.com>
2021-02-02 21:59:14 -0600

    Added TestORM connections.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,25 @@ import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 // import { UserModule } from './user/user.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [QuestModule, UsersModule],
+  // imports: [QuestModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
+
+  imports: [QuestModule, UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+
 })
 export class AppModule {}
